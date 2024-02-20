2e4aef2152b90d82d7d2020a41942fbd260a0f3a
FAIZAN HAFEEZ <mfaizan.hafeez@arhamsoft.org>
2024-02-20 16:47:38 +0500

    Main Product page Cards Design Changes

--- resources/views/frontend/pages/products.blade.php
+++ resources/views/frontend/pages/products.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.user-layout')
 
 @section('front-content')
+
     <div class="row">
-        {{-- @foreach ($categories as $products) --}}
-            @foreach ($products as $product)
-                <div class="col-md-3 mb-4">
-                    <div class="card" style="width: 100%; height: 100%;">
-                        <img class="card-img-top" src="{{ asset('storage/products/' . $product->image) }}"
-                            alt="{{ $product->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $product->name }}</h5>
-                            <p class="card-text">{{ $product->description }}</p>
-                            <p class="card-text"><strong>Price $: </strong>{{ $product->price }}</p>
-                            <a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block"
-                                role="button">Add to cart</a>
-                        </div>
+
+        @foreach ($products as $product)
+
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="{{ $product->image ? asset('storage/products/' . $product->image) : asset('storage/products/default-image.jpg') }}"
+                        alt="{{ $product->name }}" style="height: 200px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <p class="card-text"><strong>Price $: </strong>{{ $product->price }}</p>
+                        <a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block"
+                            role="button">Add to cart</a>
                     </div>
                 </div>
-            {{-- @endforeach --}}
+            </div>
+
         @endforeach
+
     </div>
+
 @endsection
